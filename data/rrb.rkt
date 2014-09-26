97a0a3b7756110666873d7d21313366562fd22d2
Cam Swords <cameronswords@gmail.com>
2014-09-26 17:18:29 -0400

    Update rrb.rkt

--- data/rrb.rkt
+++ data/rrb.rkt
@@ -4,12 +4,12 @@
          "rrb/fast.rkt")
 
 (provide/contract 
-  [rrb-get          (integer? rrb? . -> . any/c)]
-  [rrb-set          (integer? any/c rrb? . -> . rrb?)]
+  [rrb-get          (exact-nonnegative-integer? rrb? . -> . any/c)]
+  [rrb-set          (exact-nonnegative-integer? any/c rrb? . -> . rrb?)]
   [rrb-push         (any/c rrb? . -> . rrb?)]
   [rrb-concat       (rrb? rrb? . -> . rrb?)]
   [rrb-print-tree   (rrb? . -> . void?)]
-  [rrb-length       (rrb? . -> . integer?)]
+  [rrb-length       (rrb? . -> . exact-nonnegative-integer?)]
   [rrb-create-tree  (any/c . -> . rrb?)]
   [rrb?             (any/c . -> . boolean?)])
 
